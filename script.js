c1d440c07d8f41ae8d8a152ac2585ea5ce4fb2f3
YassineGhezal <85171806+Ryusuke66@users.noreply.github.com>
2023-07-16 10:19:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let form  = document.querySelector("form"),
     backCard = document.querySelector(".back"),
     description = document.querySelector(".back p"),
     re =/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    backButton = document.querySelector(".back button"),
     error = document.querySelector(".error");
 
 form.addEventListener("submit", function() {
@@ -18,3 +19,8 @@ form.addEventListener("submit", function() {
         description.innerHTML = `A confirmation email has been sent to <b>${input.value}</b>. Please open it and click the button inside to confirm your subscription.`;
     }
 });
+
+backButton.onclick = function() {
+    frontCard.classList.remove("front-flip");
+    backCard.classList.remove("back-flip");
+}
